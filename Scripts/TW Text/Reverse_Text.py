feb83183f01b9751f2966f622f278cccc0d5aa16
twardoch <adam@twardoch.com>
2021-07-05 23:49:37 +0200

    Create Reverse_Text.py

---
+++ Scripts/TW Text/Reverse_Text.py
@@ -0,0 +1,45 @@
+#FLM: Reverse Text
+# Python script for FontLab 7
+# By Adam Twardoch & Vassil Kateliev, 2021-07-05
+# Licensed at your choice under:
+# - CC-0 (public domain)
+# - MIT license, Copyright (c) 2021 Adam Twardoch, Vassil Kateliev
+
+# Get general data
+import fontlab as fl6
+import fontgate as fgt
+items = fl6.flItems
+app = fl6.flWorkspace.instance() # active workspace
+
+def processLine(sym_line):
+  """Reverses symbols in line"""
+  new_line = list(reversed(sym_line))
+  return new_line
+
+def processSymbols(symbols):
+  """Iterates through symbols, splits into lines, processes each line"""
+  symbols_new = []
+  sym_line = []
+  for symi, sym in enumerate(symbols.symbols()):
+    if sym.cr:
+      sym_line = processLine(sym_line)
+      sym_line.append(sym)
+      symbols_new += sym_line
+      sym_line = []
+    else:
+      sym_line.append(sym)
+      if symi == len(symbols) - 1:
+        sym_line = processLine(sym_line)
+  symbols_new += sym_line
+  return fl6.fgSymbolList(symbols_new)
+
+def main():
+  # Get current text block
+  canvas = app.getCanvasUnderCursor() # current canvas
+  block = canvas.textBlocks()[0] # first flTextBlock
+  fgf = block.package.fgPackage # fgPackage font associated with block
+  symbols = block.symbolList() # fgSymbolList
+  symbols = processSymbols(symbols)
+  items.requestContent(symbols, 0) # Update symbols in UI
+
+main()
